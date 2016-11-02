019766095259a1823c02de6884db8b2ccb0ff8a9
Hiron Das <hiron_cse@yahoo.com>
2016-11-02 22:02:02 +0600

    wow

--- firstPhanthomJs.js
+++ firstPhanthomJs.js
@@ -1,13 +1,25 @@
 var system = require('system');
 var url = system.args[1];
 var page = require('webpage').create();
-
-page.open("https://otctransparency.finra.org/", function(status) {
+console.log(url);
+page.open(url, function(status) {
 if ( status === "success" ) {
-	var title = page.evaluate(function() {
-		return document.title;
+	var data = page.evaluate(function() {
+		return {
+			title: document.title,
+			numberOfNodes: document.getElementsByTagName("*").length,
+			documentUrl: document.URL,
+			value: document.querySelectorAll('#submit')[0].value
+		};
 	});
-	console.log(title);
+	console.log('Page Status');
+	console.log('-------------------------------');
+	console.log('Title :'+data.title);
+	console.log('URL : '+data.documentUrl);
+	console.log('Number of Nodes: '+data.numberOfNodes);
+	console.log('Field Values: '+data.value);
+	console.log('--------------------------------');
+	this.render('test.png');
 } else {
 	console.log("Page failed to load.");
 }
